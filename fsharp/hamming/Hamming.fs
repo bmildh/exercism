ac51a25ece5e4ba0c2d73a18d3148d8c00764cc4
Benny Mildh <bmildh@gmail.com>
2021-03-17 20:10:52 +0100

    Initial commit

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,12 @@
+﻿module Hamming
+open System
+
+let distance (strand1: string) (strand2: string): int option = 
+    if(String.length strand1 = String.length strand2) then
+        match (strand1, strand2) with
+        | ("",_) -> Some(0)
+        | (_,"") -> Some(0)
+        | (_,_) -> Seq.fold2 (fun acc c1 c2 -> if(c1 <> c2) then acc+1 else acc) 0 strand1 strand2 |> Some
+    else
+        None
+
