3acfd423b71caa806fa7a235bb56dbd027d09a45
Benny Mildh <bmildh@gmail.com>
2021-03-17 20:12:04 +0100

    Initial commit

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,17 @@
+﻿module Pangram
+open System
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz" 
+
+// This uses Set's, a very nice feature of the language. 
+let isPangram (input: string): bool = 
+    let alphabet = "abcdefghijklmnopqrstuvwxyz" 
+    input 
+    |> Seq.toList
+    |> List.map Char.ToLower
+    |> Set.ofList
+    |> Set.intersect (Set.ofList (Seq.toList alphabet))  
+    |> Set.count
+    |> function
+        | 26 -> true
+        | _  -> false
