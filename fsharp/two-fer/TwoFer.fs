79fa0883c6e615724af9ec167334effc2275f0c4
Benny Mildh <bmildh@gmail.com>
2021-03-17 20:09:58 +0100

    Initial commit

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,10 @@
+﻿module TwoFer
+
+
+let twoFer (input: string option): string = 
+    match input with
+    | Some(input) -> $"One for {input}, one for me."
+    | None -> "One for you, one for me."
+
+
+//failwith "You need to implement this function."
