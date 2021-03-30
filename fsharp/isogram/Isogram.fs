12491d00c4b70b008ffe72c4713390b244f04e02
Benny Mildh <bmildh@gmail.com>
2021-03-30 13:28:42 +0200

    First attempt. Recursive.

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,3 +1,23 @@
-﻿module Isogram
+﻿module Isogram 
+open System
 
-let isIsogram str = failwith "You need to implement this function."
+// I had add the type annotation here, otherwise there was a type mismatch from the test 
+// suite. Could not figure out another way.
+let isIsogram (str:string) =
+
+    let rec recIsIsogram input =
+
+        match input with
+        | [] -> true
+        | ' '::tail  -> recIsIsogram tail
+        | '-':: tail -> recIsIsogram tail
+        | head::tail ->
+            match (List.contains head tail) with
+            | true -> false
+            | false -> recIsIsogram tail
+
+    str
+    |> Seq.toList
+    |> List.map Char.ToLower
+    |> recIsIsogram
+             
